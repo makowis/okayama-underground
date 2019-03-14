d2e481b5bd768fc187a6797e7859cd568a9e9f47
Makoto Henmi <mako.mail.g@gmail.com>
2019-03-14 20:56:51 +0900

    （笑）

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+      <div className={styles.app}>
+        <header className={styles.header}>
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            className={styles.link}
+            href="https://www.yahoo.co.jp/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            ENTER
           </a>
         </header>
       </div>
