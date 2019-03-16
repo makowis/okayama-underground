94a29e156b4792830cf875db8745a92553037118
Makoto Henmi <mako.mail.g@gmail.com>
2019-03-16 10:40:29 +0900

    今北産業
    スタイルを調整

--- src/components/organisms/OugDummySpiralEnter.tsx
+++ src/components/organisms/OugDummySpiralEnter.tsx
@@ -4,20 +4,24 @@ import styles from './OugDummySpiralEnter.module.scss';
 
 class OugDummySpiralEnter extends Component {
   render() {
-    return [
-      styles.link1,
-      styles.link2,
-      styles.link3,
-      styles.link4,
-      styles.link5,
-      styles.link6,
-      styles.link7,
-      styles.link8,
-      styles.link9,
-      styles.link10
-    ].map(value => {
-      return <OugDummyEnterLink key={value} className={value} />;
-    });
+    return (
+      <div className={styles.container}>
+        {[
+          styles.link1,
+          styles.link2,
+          styles.link3,
+          styles.link4,
+          styles.link5,
+          styles.link6,
+          styles.link7,
+          styles.link8,
+          styles.link9,
+          styles.link10
+        ].map(value => {
+          return <OugDummyEnterLink key={value} className={value} />;
+        })}
+      </div>
+    );
   }
 }
 
