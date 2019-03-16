5ff3c56c8dabbf78e54db8d08023c1a4d2dcdb5f
Makoto Henmi <mako.mail.g@gmail.com>
2019-03-16 11:24:02 +0900

    age
    storybookを追加

---
+++ stories/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import OugH1 from '../src/components/atoms/OugH1'
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('OugH1', module)
+    .add('with text', () => <OugH1 value = "Hello World" />)
