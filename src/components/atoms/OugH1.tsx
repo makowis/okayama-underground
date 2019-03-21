85fe6d14d643b981587200e242094910e203c5c3
Tomohiko Himura <eiel.hal@gmail.com>
2019-03-22 01:02:32 +0900

    refact: use FunctionComponent

--- src/components/atoms/OugH1.tsx
+++ src/components/atoms/OugH1.tsx
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import styles from './OugH1.module.scss';
 
-class OugH1 extends Component<{ value: string }> {
-  render() {
-    return <h1 className={styles.h1}>{this.props.value}</h1>;
-  }
-}
+const OugH1: FunctionComponent<{ value: string }> = ({ value }) => (
+  <h1 className={styles.h1}>{value}</h1>
+);
 
 export default OugH1;
--- src/components/organisms/OugDummySpiralEnter.tsx
+++ src/components/organisms/OugDummySpiralEnter.tsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import OugDummyEnterLink from '../atoms/OugDummyEnterLink';
 import styles from './OugDummySpiralEnter.module.scss';
 
-class OugDummySpiralEnter extends Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        {[
-          styles.link1,
-          styles.link2,
-          styles.link3,
-          styles.link4,
-          styles.link5,
-          styles.link6,
-          styles.link7,
-          styles.link8,
-          styles.link9,
-          styles.link10
-        ].map(value => {
-          return <OugDummyEnterLink key={value} className={value} />;
-        })}
-      </div>
-    );
-  }
-}
+const styleLinks = [
+  styles.link1,
+  styles.link2,
+  styles.link3,
+  styles.link4,
+  styles.link5,
+  styles.link6,
+  styles.link7,
+  styles.link8,
+  styles.link9,
+  styles.link10
+];
+
+const OugDummyEnterLinkWrapper: FunctionComponent<string> = value => (
+  <OugDummyEnterLink key={value} className={value} />
+);
+
+const OugDummySpiralEnter: FunctionComponent = () => (
+  <div className={styles.container}>
+    {styleLinks.map(OugDummyEnterLinkWrapper)}
+  </div>
+);
 
 export default OugDummySpiralEnter;
